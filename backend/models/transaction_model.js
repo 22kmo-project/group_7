bc8764816e3c522d2f8416a236b661260250e46a
justiina <t1roai00@students.oamk.fi>
2022-11-22 19:39:06 +0200

    uusi dump missä foreign keyt mukana ja korjattu transaction

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -11,8 +11,8 @@ const transaction = {
   
   add: function(add_data, callback) {
     return db.query(
-      'insert into transaction (id_account,id_card,event_date,event,balance) values(?,?,?,?,?)',
-      [add_data.id_account, add_data.id_card, add_data.transaction_date, add_data.transaction, add_data.balance],
+      'insert into transaction (id_account,id_card,transaction_date,transaction,amount) values(?,?,?,?,?)',
+      [add_data.id_account, add_data.id_card, add_data.transaction_date, add_data.transaction, add_data.amount],
       callback
     );
   },
@@ -23,8 +23,8 @@ const transaction = {
   
   update: function(id, update_data, callback) {
     return db.query(
-      'update transaction set id_account=?,id_card=?,event_date=?,event=?,balance=? where id_transaction=?',
-      [update_data.id_account, update_data.id_card, update_data.transaction_date, update_data.transaction, update_data.balance, id],
+      'update transaction set id_account=?,id_card=?,transaction_date=?,transaction=?,amount=? where id_transaction=?',
+      [update_data.id_account, update_data.id_card, update_data.transaction_date, update_data.transaction, update_data.amount, id],
       callback
     );
   }
