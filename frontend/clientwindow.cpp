0c1b992a3a6522758254ac7d5428e63d03f05881
Essi Nissinen <t1nies@students.oamk.figit config --global user.email t1nies@students.oamk.fi>
2022-11-27 19:45:53 +0200

    näytä saldo hakee tilitapahtumat ja näyttää muut tarvittavat tiedot

--- frontend/clientwindow.cpp
+++ frontend/clientwindow.cpp
@@ -19,6 +19,8 @@ ClientWindow::~ClientWindow()
     delete ui;
     delete objectDrawWindow;
     objectDrawWindow=nullptr;
+    delete objectBalanceWindow;
+    objectBalanceWindow=nullptr;
 }
 
 void ClientWindow::setWebToken(const QByteArray &newWebToken)
@@ -29,7 +31,6 @@ void ClientWindow::setWebToken(const QByteArray &newWebToken)
 void ClientWindow::on_button_nayta_saldo_clicked()//ESSI
 {
     objectBalanceWindow = new BalanceWindow(webToken,myCardId);
-    objectBalanceWindow->setWebToken("Bearer "+response_data);
     objectBalanceWindow->show();
 
 }
