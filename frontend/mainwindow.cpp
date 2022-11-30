3576b48887a549de170f8b1ef6dd31017eb08fee
justiina <t1roai00@students.oamk.fi>
2022-11-30 15:13:11 +0200

    numeronäppäimet mainwindowssa

--- frontend/mainwindow.cpp
+++ frontend/mainwindow.cpp
@@ -7,6 +7,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     //QWidget::showMaximized(); //Näytetään ikkuna kokonäytöllä
+    ui->label_info->setText("Anna kortin numero ja paina ok");
+    ok_count=1;
 }
 
 MainWindow::~MainWindow()
@@ -16,25 +18,34 @@ MainWindow::~MainWindow()
     objectClientWindow=nullptr;
 }
 
-
-void MainWindow::on_button_login_clicked()
+void MainWindow::on_btn_ok_clicked()
 {
-    id_card=ui->text_id_card->text();
-    pin=ui->text_pin->text();
+    switch(ok_count) {
+    case 1:
+        id_card=ui->lineEdit->text();
+        ui->lineEdit->clear();
+        ui->label_info->setText("Anna pin-koodi ja paina ok");
+        break;
+    case 2:   
+        pin=ui->lineEdit->text();
+        ui->label_info->setText("Varmista kirjautuminen painamalla ok");
+        break;
+    case 3:
+        QJsonObject jsonObj;
+        jsonObj.insert("id_card", id_card);
+        jsonObj.insert("pin", pin);
 
-    QJsonObject jsonObj;
-    jsonObj.insert("id_card", id_card);
-    jsonObj.insert("pin", pin);
+        QString site_url=MyURL::getBaseUrl()+"/login";
+        QNetworkRequest request((site_url));
+        request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-    QString site_url=MyURL::getBaseUrl()+"/login";
-    QNetworkRequest request((site_url));
-    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+        loginManager = new QNetworkAccessManager(this);
+        connect(loginManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(loginSlot(QNetworkReply*)));
 
-    loginManager = new QNetworkAccessManager(this);
-    connect(loginManager, SIGNAL(finished(QNetworkReply*)), this, SLOT(loginSlot(QNetworkReply*)));
-
-    reply = loginManager->post(request, QJsonDocument(jsonObj).toJson());
-
+        reply = loginManager->post(request, QJsonDocument(jsonObj).toJson());
+        break;
+    }
+    ok_count++;
 }
 
 void MainWindow::loginSlot(QNetworkReply *reply)
@@ -45,24 +56,21 @@ void MainWindow::loginSlot(QNetworkReply *reply)
     qDebug()<<test;
 
     if(response_data.length()==0){
-        ui->text_id_card->clear();
-        ui->text_pin->clear();
+        ui->lineEdit->clear();
         ui->label_info->setText("Palvelin ei vastaa!");
     }
     else{
 
     if(QString::compare(response_data,"-4078")==0){
-        ui->text_id_card->clear();
-        ui->text_pin->clear();
+        ui->lineEdit->clear();
         ui->label_info->setText("Virhe tietokantayhteydessä!");
     }
 
     else{
 
         if(test==0){
-            ui->text_id_card->clear();
-            ui->text_pin->clear();
-            ui->label_info->setText("Kortinnumero ja pin-koodi eivät täsmää!");
+            ui->lineEdit->clear();
+            ui->label_info->setText("Kortin numero ja pin-koodi eivät täsmää!");
         }
 
         else {
@@ -79,8 +87,68 @@ void MainWindow::loginSlot(QNetworkReply *reply)
 }
 
 
-void MainWindow::on_button_exit_clicked()
+void MainWindow::on_btn_clear_clicked()
+{
+    qApp->quit();
+    QProcess::startDetached(qApp->arguments()[0], qApp->arguments());
+}
+
+
+void MainWindow::on_btn_close_clicked()
 {
     QApplication::closeAllWindows();
 }
+
+
+void MainWindow::on_btn_1_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "1");
+}
+
+void MainWindow::on_btn_2_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "2");
+}
+
+void MainWindow::on_btn_3_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "3");
+}
+
+
+void MainWindow::on_btn_4_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "4");
+}
+
+void MainWindow::on_btn_5_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "5");
+}
+
+void MainWindow::on_btn_6_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "6");
+}
+
+void MainWindow::on_btn_7_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "7");
+}
+
+void MainWindow::on_btn_8_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "8");
+}
+
+void MainWindow::on_btn_9_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "9");
+}
+
+void MainWindow::on_btn_0_clicked()
+{
+    ui->lineEdit->setText(ui->lineEdit->text()+ "0");
+}
+
 
